744725dec3db71a3508e5a37b77ca5a281b39fd5
Manon <manon.chp68@gmail.com>
2025-06-23 15:02:51 +0200

    ajout search bar et paginator bundle

---
+++ src/Form/SearchTypeForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Model\SearchData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class SearchTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('q', TextType::class, [
+                'label' => 'Rechercher',
+                'attr' => [
+                    'placeholder' => 'Rechercher',
+                    'class' => 'form-control',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SearchData::class,
+            'method' => 'GET',
+            'csrf_protection' => false
+        ]);
+    }
+}
---
+++ src/Model/SearchData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model;
+
+class SearchData
+{
+    /**
+     * @var string
+     */
+    public ?string $q = '';
+
+    /**
+     * @var int
+     */
+    public ?int $page = 1;
+}
