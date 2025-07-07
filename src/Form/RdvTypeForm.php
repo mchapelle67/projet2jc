6114742209ed3756a51256ad7f0ddae46d922751
Manon <manon.chp68@gmail.com>
2025-07-07 17:07:00 +0200

    création du calendrier côté client prise de rdv

--- src/Form/RdvTypeForm.php
+++ src/Form/RdvTypeForm.php
@@ -12,16 +12,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class RdvTypeForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date_rdv', null, [
-                'label' => 'Date du rendez-vous*',
-                'widget' => 'single_text',
-            ])
+            ->add('date_rdv', HiddenType::class)
             ->add('nom', TextType::class, [
                 'label' => 'Nom*',
             ])
@@ -43,6 +42,19 @@ class RdvTypeForm extends AbstractType
             ])
             ->add('vehicule', VehiculeTypeForm::class) // ajout du form vehicule pour l'imbriquer
         ;
+
+        // transformer le champ date_rdv pour gérer l'affichage et la soumission
+        // on utilise un CallbackTransformer pour transformer la valeur entre l'affichage et la soumission
+         $builder->get('date_rdv')->addModelTransformer(new CallbackTransformer(
+            function ($dateAsObject) {
+                // transforme l'objet en string pour le champ (affichage)
+                return $dateAsObject instanceof \DateTimeInterface ? $dateAsObject->format('Y-m-d\TH:i') : $dateAsObject;
+            },
+            function ($dateAsString) {
+                // transforme la string en DateTimeImmutable pour l'entité
+                return $dateAsString ? \DateTimeImmutable::createFromFormat('Y-m-d\TH:i', $dateAsString) : null;
+            }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
